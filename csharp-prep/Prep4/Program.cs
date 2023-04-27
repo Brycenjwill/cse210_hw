074019711e24d24383b51147d3480e90afa3b3de
Brycen Williams <wil21062@byui.edu>
2023-04-27 14:54:32 -0600

    In class work for Prep 3 and Prep 4.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        
+
+        float average = 0;  
+        int length = 0;  
+        var numbers = new List<int>();
+        while(true){
+            Console.WriteLine("Choose a number to add: ");
+            int Input = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Would you like to add another number? (Y/N)");
+
+            string choice = Console.ReadLine();
+
+            numbers.Add(Input);
+            choice = choice.ToLower();
+
+            
+
+            if (choice[0] == 'n'){
+                break;
+            }
+
+
+        }
+        int total = 0;
+        foreach (var num in numbers) {
+            total += num;
+        }
+        length = numbers.Count();
+        average = (float)total / (float)length;
+
+        Console.WriteLine($"The total is {total}.");
+        Console.WriteLine($"The average is {average}.");
     }
+    
 }
