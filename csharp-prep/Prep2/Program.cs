79b2e59b7ecfd1cc8006fa5deca0e145c1cbba5e
Brycen Williams <wil21062@byui.edu>
2023-04-26 19:21:41 -0600

    Updated prep 1 and 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        char letter = 'F';
+        Console.WriteLine("What is your grade percentage? ");
+        int percent = Convert.ToInt32(Console.ReadLine());
+
+        if (percent >= 70) {
+
+            if (percent >= 80){
+
+            if (percent >= 90){
+                letter = 'A';
+            }
+            else {
+                letter = 'B';
+            }
+            }
+
+
+            else {
+                letter = 'C';
+            }
+            Console.WriteLine($"You Passed! You got a {letter}.");
+        }
+        else{
+            if (percent < 60){
+                    letter = 'F';
+            }
+            else{
+                letter = 'D';
+            }
+            Console.WriteLine($"You failed. You got a {letter}.");
+            }
+        
+        }
+
     }
-}
