edc49d9875d31b7e6311da9577afe0fb8013f611
Brycen Williams <brycenjwill@gmail.com>
2023-05-03 19:31:15 -0600

    Finished wk3 prep assignment

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1.job_title = "Software Engineer";
+        job1.company = "Intel";
+        job1.start_year = 2011;
+        job1.end_year = 2022;
+
+        Job job2 = new Job();
+        job2.job_title = "Physical Therapist";
+        job2.company = "The Hospital";
+        job2.start_year = 2011;
+        job2.end_year = 2019;
+        
+        job1.display_info();
+        job2.display_info();
+
+        resume myResume = new resume();
+        myResume.name = "Brycen Williams";
+
+        myResume.jobs.Add(job1);
+        myResume.jobs.Add(job2);
+
+        myResume.display_info();
+
     }
 }
