6c0b1afd26b85eb6e98f485f7fa1e69c85310978
Brycen Williams <wil21062@byui.edu>
2023-05-09 18:12:01 -0600

    Prompt Generator

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        bool on = true;
+
+        while(on == true)
+        {
+            Console.WriteLine("Welcome to teh Journal Program!");
+            Console.WriteLine("Please selct one of the following choices:");
+            Console.WriteLine("1. Write\n2. Display\n3. Load\n4. Save\n5. Quit");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            if (choice == 1){ // Write
+                break;
+            }
+            else if(choice == 2){ // Display
+                break;
+            }
+            else if (choice == 3){ // Load
+                break;
+            }
+            else if (choice == 4){ //Save
+                break;
+            }
+            else if (choice == 5){ //Quit
+                on = false;
+            }
+        }
     }
 }
---
+++ prove/Develop02/PromptGenerator.cs
@@ -0,0 +1,22 @@
+public class Prompt
+{
+    public List<String> Prompts = new List<String> {
+        "What was the best part of your day?",
+        "What was something you were grateful for today?",
+        "Where did you go today?",
+        "What was something fun you did today?",
+        "Did you talk to anyone interesting today?",
+        "Did you meet anyone new today?",
+        "What do you want to do tomorrow"
+
+    };
+    public Random numberGenerator = new Random();
+
+
+public string PromptGen(){
+    int index = numberGenerator.Next(Prompts.Count);
+    
+    return Prompt;
+}
+}
+
