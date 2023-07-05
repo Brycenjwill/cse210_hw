f426455972bec575483b99dbe0b90129f372c7db
Brycen Williams <wil21062@byui.edu>
2023-07-05 14:07:22 -0600

    Merged

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,8 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        string scripture = "And my father dwelt in a atent.";
-        Reference reference = new Reference("2", "15");
+        bool on = true;
+ 
+
+        Reference refe = new Reference("1 Nephi", 2, 15);
+        Scripture scripture = new Scripture("And my father dwelt in a tent.");
+        string refer = refe.getRef();
+        int i = 0;
+        int runs = scripture.getArray()-1;
+        Console.Clear();
+        scripture.display(refer);
+
+        while(on == true){
+            string choice = Console.ReadLine();
+            Console.Clear();
         
+            scripture.randomNumGen();
+
+
+            scripture.display(refer);
+            if (choice == "quit"){
+                on = false;
+            }
+            if (i == runs){
+                on = false;
+            }
+            i += 1;
+        }
     }
 }
--- prove/Develop03/Reference.cs
+++ prove/Develop03/Reference.cs
@@ -2,12 +2,15 @@ public class Reference
 {
     private string reference;
 
-public Reference(string chapter, string range){
-    reference = $"{chapter}:{range}";
-    Console.WriteLine(reference);
+public Reference(string book, int chapter, int range){
+    reference = $"{book} {chapter}:{range}";
+
 }
-public Reference(string chapter, string beginRange, string endRange){
-    reference = $"{chapter}:{beginRange}-{endRange}";
+public Reference(string book, int chapter, int beginRange, int endRange){
+    reference = $"{book} {chapter}:{beginRange}-{endRange}";
+}
+public string getRef(){
+    return reference;
 }
 
 }
